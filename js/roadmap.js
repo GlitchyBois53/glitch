2056645ed903c0fd18f3d9ce37d3359fc8ec854e
frbarbre <fr.barbre@gmail.com>
2023-04-16 00:42:46 +0200

    music player added

--- js/roadmap.js
+++ js/roadmap.js
@@ -26,3 +26,37 @@ faders.forEach((fader) => {
 sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
 });
+
+var podcast = document.getElementById("audio")
+var play = document.getElementById("play")
+var pause = document.getElementById("pause")
+var elapsed = document.getElementById("progress");
+
+play.addEventListener("click", () => {
+    podcast.play()
+    play.style.display = "none"
+    pause.style.display = "block"
+    setTimeout(function () {
+        elapsed.classList.add("blue-glow")
+    }, 500);
+    
+})
+
+pause.addEventListener("click", () => {
+    podcast.pause()
+    play.style.display = "block"
+    pause.style.display = "none"
+})
+
+setInterval(function(){
+    var percentage = audio.currentTime / audio.duration;
+    var width = (percentage * 100);
+    elapsed.style.width = width + "%"
+    
+    if (width == 100){
+        podcast.pause()
+        play.style.display = "block"
+        pause.style.display = "none"
+        elapsed.classList.remove("blue-glow")
+    }
+}, 33);
